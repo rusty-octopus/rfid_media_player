f00d308dddf9553180c4d5046db955cb48648fb0
rusty-octopus <rustyoctopus@posteo.org>
2020-10-20 21:31:30 +0200

    Detached kernel driver and read from device.

--- usb_reader_test/src/main.rs
+++ usb_reader_test/src/main.rs
@@ -18,19 +18,26 @@ fn main() {
     let active_configuration = device_handle.active_configuration();
     println!("Active configuraton: {:?}", active_configuration);
     //device_handle.reset().unwrap();
-    //device_handle.claim_interface(0).unwrap();
-    let mut buffer:[u8;100] = [0;100];
-    let read_result = device_handle.read_interrupt(0x81, & mut buffer, Duration::from_secs(10));
-    if read_result.is_ok() {
-      let value = String::from_utf8(Vec::from(buffer));
-      if value.is_ok() {
-        let value = value.unwrap();
-      println!("Value: {}", value);
+    let mut buffer:[u8;3] = [0;3];
+    println!("Has kernel driver: {:?}", device_handle.kernel_driver_active(0));
+    println!("Detach kernel driver? {:?}", device_handle.detach_kernel_driver(0));
+    device_handle.claim_interface(0).unwrap();
+    for _ in 0..15 {
+      let read_result = device_handle.read_interrupt(0x81, & mut buffer, Duration::from_secs(10));
+      if read_result.is_ok() {
+        println!("Read result: {:?}", read_result);
+        println!("Buffer: {:?}", buffer);
+        let value = String::from_utf8(Vec::from(buffer));
+        if value.is_ok() {
+          let value = value.unwrap();
+        println!("Value: {}", value);
+        } else {
+          let error = value.unwrap_err();
+          println!("{:?}", error);
+        }
       } else {
-        let error = value.unwrap_err();
-        println!("{:?}", error);
+        println!("Could not read from the device: {:?}", read_result.unwrap_err());
       }
-    } else {
-      println!("Could not read from the device: {:?}", read_result.unwrap_err());
-    }
+  }
+  device_handle.attach_kernel_driver(0);
 }
