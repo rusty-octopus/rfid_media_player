b3999487e026145134b35dbdee0f8342674630af
rusty-octopus <rustyoctopus@posteo.org>
2020-10-29 22:09:44 +0100

    Formatted code.

--- rfid_reader/src/keymap.rs
+++ rfid_reader/src/keymap.rs
@@ -1,19 +1,19 @@
 use crate::keymaperror::KeyMapError;
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
 pub(crate) enum Key {
-  Digit(char),
-  Enter,
+    Digit(char),
+    Enter,
 }
 pub(crate) trait KeyMap {
-  fn map(&self, key:u8) -> Result<Key,KeyMapError>;
+    fn map(&self, key: u8) -> Result<Key, KeyMapError>;
 }
 
 impl Into<char> for Key {
-  fn into(self) -> char {
-    match self {
-      Self::Digit(c) => c,
-      Self::Enter => 'Z',
+    fn into(self) -> char {
+        match self {
+            Self::Digit(c) => c,
+            Self::Enter => 'Z',
+        }
     }
-  } 
 }
--- rfid_reader/src/keymaperror.rs
+++ rfid_reader/src/keymaperror.rs
@@ -1,35 +1,42 @@
-use std::fmt::{Display, Formatter, Result};
 use std::error::Error;
-#[derive(Debug,PartialEq)]
+use std::fmt::{Display, Formatter, Result};
+#[derive(Debug, PartialEq)]
 pub(crate) enum KeyMapError {
-  KeyNotExisting(String)
+    KeyNotExisting(String),
 }
 
 impl KeyMapError {
-  pub(crate) fn from_key_not_existing(key: u8) -> KeyMapError {
-    Self::KeyNotExisting(format!("There is no key char for u8 value \"{}\".", key))
-  }
+    pub(crate) fn from_key_not_existing(key: u8) -> KeyMapError {
+        Self::KeyNotExisting(format!("There is no key char for u8 value \"{}\".", key))
+    }
 }
 
 impl Display for KeyMapError {
-  fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-    match self {
-      Self::KeyNotExisting(msg) => write!(f,"{}", msg)
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            Self::KeyNotExisting(msg) => write!(f, "{}", msg),
+        }
     }
-  }
 }
 
 impl Error for KeyMapError {}
 
 #[cfg(test)]
 mod tests {
-  use super::*;
-  #[test]
-  fn test_key_not_existing() {
-    let error = KeyMapError::from_key_not_existing(17);
-    let expected_error_string: String = format!("There is no key char for u8 value \"{}\".", 17);
-    assert_eq!(KeyMapError::KeyNotExisting(expected_error_string.clone()), error);
-    assert_eq!(format!("KeyNotExisting({:?})", expected_error_string), format!("{:?}", error));
-    assert_eq!(expected_error_string, format!("{}", error));
-  }
+    use super::*;
+    #[test]
+    fn test_key_not_existing() {
+        let error = KeyMapError::from_key_not_existing(17);
+        let expected_error_string: String =
+            format!("There is no key char for u8 value \"{}\".", 17);
+        assert_eq!(
+            KeyMapError::KeyNotExisting(expected_error_string.clone()),
+            error
+        );
+        assert_eq!(
+            format!("KeyNotExisting({:?})", expected_error_string),
+            format!("{:?}", error)
+        );
+        assert_eq!(expected_error_string, format!("{}", error));
+    }
 }
