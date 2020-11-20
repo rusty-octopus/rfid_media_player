f4c6ac8fe37980d89721f114ddf7fb5f266957fe
rusty-octopus <rustyoctopus@posteo.org>
2020-11-20 22:55:59 +0100

    Added blanket implementations for initialize and deinitialzed. Replaced configure_device.

--- rfid_reader/src/humbleusbdevice.rs
+++ rfid_reader/src/humbleusbdevice.rs
@@ -15,10 +15,19 @@ pub trait HumbleUsbDevice {
     fn release_interface(&mut self) -> Result<(), Error>;
     fn set_active_configuration(&mut self) -> Result<(), Error>;
     fn set_alternate_setting(&mut self) -> Result<(), Error>;
-    fn configure_device(&mut self) -> Result<(), Error> {
+    fn initialize(&mut self) -> Result<(), Error> {
+        if self.has_attached_kernel_driver() {
+            self.detach_kernel_driver()?;
+        }
         self.set_active_configuration()?;
         self.claim_interface()?;
         self.set_alternate_setting()?;
+        Ok(())
+    }
+    fn deinitialize(&mut self) -> Result<(), Error> {
+        if self.has_attached_kernel_driver() {
+            self.attach_kernel_driver()?;
+        }
         Ok(())
     }
 }
