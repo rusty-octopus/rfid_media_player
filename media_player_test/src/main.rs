165a2a5485f051e6824369e4e25597e367d0f61a
rusty-octopus <rustyoctopus@posteo.org>
2020-12-20 21:49:01 +0100

    Added more possibilities to actually play and stop songs.

--- media_player_test/src/main.rs
+++ media_player_test/src/main.rs
@@ -1,20 +1,21 @@
 use rodio::Decoder;
 use rodio::Sink;
 use rodio::Source;
+use rodio::OutputStreamHandle;
+use rodio::OutputStream;
 use std::fs::File;
 use std::io::BufReader;
 use std::thread::sleep;
 use std::time::Duration;
 
-fn main() {
-    println!("Hello, world!");
+fn do_play() {
     let (stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
     //let sink = Sink::try_new(&stream_handle).unwrap();
     let file = File::open("sound.flac").unwrap();
     let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
     //let (sink, output_queue) = Sink::new_idle();
-    sink.set_volume(1.0);
+    sink.set_volume(0.1);
     //let sink = stream_handle.play_raw(source.convert_samples()).unwrap();
     //let source = rodio::source::SineWave::new(440);
     //sink.append(source);
@@ -29,18 +30,77 @@ fn main() {
         let is_playing = !sink.empty();
         println!("Still playing? {}", is_playing);
         println!("Is paused? {}", sink.is_paused());
-        if counter > 5 {
+        if counter == 5 {
             sink.stop();
             let is_playing = !sink.empty();
             println!("Still playing? {}", is_playing);
         }
         if counter == 7 {
             println!("Restart");
-            let file2 = File::open("sound.flac").unwrap();
+            let file2 = File::open("sound2.wav").unwrap();
             let source2 = rodio::Decoder::new(BufReader::new(file2)).unwrap();
             sink.append(source2);
-            //sink.play();
+            sink.play();
         }
         counter += 1;
     }
+}
+
+fn play(path: &str) -> (Sink, OutputStream, OutputStreamHandle) {
+    let (stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+
+    let file = File::open(path).unwrap();
+    let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+
+    let sink = Sink::try_new(&stream_handle).unwrap();
+    sink.set_volume(0.1);
+    sink.append(source);
+    sink.play();
+    (sink, stream, stream_handle)
+}
+
+fn do_play2() {
+    let (mut sink, mut stream, mut stream_handle) = play("sound.flac");
+
+    let mut counter = 1;
+
+    loop {
+        let is_playing = !sink.empty();
+        println!("Still playing? {}", is_playing);
+        if counter == 5 {
+            sink.stop();
+            println!("Stopped");
+        }
+        if counter == 7 {
+            let (sink_new, stream_new, stream_handle_new) = play("sound2.wav");
+            sink = sink_new;
+            stream = stream_new;
+            stream_handle = stream_handle_new;
+            println!("Started new sound");
+        }
+        if counter == 17 {
+            println!("Should have stopped playing");
+            println!("Still playing? {}", is_playing);
+            break;
+        }
+        sleep(Duration::from_secs(2));
+        counter += 1;
+    }
+
+    println!("sound over");
+}
+
+fn main() {
+    do_play2();
+    //let (sink, stream, stream_handle) = play("sound.flac");
+    // let path ="sound.flac";
+    // let (stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+
+    // let file = File::open(path).unwrap();
+    // let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+
+    // let sink = Sink::try_new(&stream_handle).unwrap();
+    // sink.set_volume(0.1);
+    // sink.append(source);
+    loop {}
 }
